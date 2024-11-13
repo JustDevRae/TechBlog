f8d56df713d4d73a293e4cc496324360249dc2d5
JustDevRae <ksrae165@gmail.com>
2024-11-13 15:57:31 +0900

    feat: install @next/mdx @types/mdx @mdx-js/loader more install: @mdx-js/react

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // NOTE: MDX 및 기타 파일 확장자를 페이지로 인식하도록 설정
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // NOTE: 추후 remark 및 rehype 플러그인을 추가할 예정
+});
+
+
+export default withMDX(nextConfig);
