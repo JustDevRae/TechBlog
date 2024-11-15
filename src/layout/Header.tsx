460d02ad83c181e61f30997967344195a8b4e5c5
JustDevRae <ksrae165@gmail.com>
2024-11-15 16:38:17 +0900

    chore: setting typescript/eslint

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -4,7 +4,8 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <Link href="/">Home</Link> | <Link href="/aboutme">About Me</Link> |{" "}
+        <Link href="/">Home</Link>
+        <Link href="/aboutme">About Me</Link>
         <Link href="/tags">Tags</Link>
       </nav>
     </header>
