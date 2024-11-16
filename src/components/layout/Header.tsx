1a67d6d18ba4fbd71f3f475371da38ac627b1cf4
JustDevRae <ksrae165@gmail.com>
2024-11-16 16:17:59 +0900

    chore: Install Prettier Tailwind CSS plugin

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,12 +3,12 @@ import ThemeToggle from "../ThemeToggle";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900 shadow-md">
+    <header className="sticky top-0 z-50 bg-white shadow-md dark:bg-gray-900">
       <div className="flex items-center justify-between">
         <Link href="/" className="text-lg font-bold hover:opacity-80">
           Rae.
         </Link>
-        <nav className="hidden md:flex space-x-4">
+        <nav className="hidden space-x-4 md:flex">
           <Link href="/aboutme" className="hover:text-blue-500">
             About Me
           </Link>
