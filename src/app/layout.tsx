26d2426f545beba435bea246fc2bae4007ad977b
JustDevRae <ksrae165@gmail.com>
2024-11-16 12:50:46 +0900

    chore: fix file location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./globals.css";
-import Header from "@/layout/Header";
-import Footer from "@/layout/Footer";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 export default function RootLayout({
   children,
