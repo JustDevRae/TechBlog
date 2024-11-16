ed2f6bba38e12ca28cd7fdac5d1ee7f36da778a0
JustDevRae <ksrae165@gmail.com>
2024-11-16 21:55:49 +0900

    design: modifty ThemeToggle Image

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -2,6 +2,9 @@
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import Light from "../../public/images/light.svg";
+import Dark from "../../public/images/dark.svg";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -13,10 +16,14 @@ export default function ThemeToggle() {
 
   return (
     <button
-      className="p-2 bg-gray-200 dark:bg-gray-800 rounded-md hover:ring-2 hover:ring-gray-300"
+      className="rounded-md bg-gray-200 p-2 hover:ring-2 hover:ring-gray-300 dark:bg-gray-200"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {theme === "dark" ? "🌙" : "🎇"}
+      {theme === "dark" ? (
+        <Image src={Dark} alt="darkmode" width={20} height={20} />
+      ) : (
+        <Image src={Light} alt="lightmode" width={20} height={20} />
+      )}
     </button>
   );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,16 +3,16 @@ import ThemeToggle from "../ThemeToggle";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 bg-white shadow-md dark:bg-gray-900">
-      <div className="flex items-center justify-between">
-        <Link href="/" className="text-lg font-bold hover:opacity-80">
+    <header className="sticky top-0 z-50 bg-white p-5 shadow-md dark:bg-gray-900">
+      <div className="flex items-center justify-between text-lg font-bold">
+        <Link href="/" className="text-gray-900 dark:text-white">
           Rae.
         </Link>
-        <nav className="hidden space-x-4 md:flex">
-          <Link href="/aboutme" className="hover:text-blue-500">
+        <nav className="flex items-center space-x-4">
+          <Link href="/aboutme" className="text-gray-900 dark:text-white">
             About Me
           </Link>
-          <Link href="/tags" className="hover:text-blue-500">
+          <Link href="/tags" className="text-gray-900 dark:text-white">
             Tags
           </Link>
           <ThemeToggle />
